050cec64469dc4150514d78d4931ac7470c0e877
Edube1008 <christina.njongwe@gmail.com>
2025-03-03 15:44:51 -0700

    key

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "backend-dev-git-terraform-bucket"  # S3 Bucket for state file
-    key            = "dev/terraform.tfstate"             # State file path in S3
+    key            = "env/terraform.tfstate"             # State file path in S3
     region         = "ca-central-1"                      # Region of S3 bucket
     dynamodb_table = "terraform-dynamo-db-dev-table"          # DynamoDB table for locking
     encrypt        = true
